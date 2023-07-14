ba8dc7944b6ee2ff10599d7db795f1d11667a866
melane6 <mnavaratnarajah6@gmail.com>
2023-07-14 11:18:02 +0100

    Update tests for history fragment

--- app/src/androidTest/java/de/unisaarland/loladrives/HistoryFragmentTest.kt
+++ app/src/androidTest/java/de/unisaarland/loladrives/HistoryFragmentTest.kt
@@ -1,6 +1,7 @@
 package de.unisaarland.loladrives
 
 
+import android.util.Log
 import java.io.File
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.*
@@ -18,35 +19,39 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class HistoryFragmentTest {
-    lateinit var file : File;
-    lateinit var data : String;
 
     @Before
     fun setUp() {
-        data = "Sample Data"
         val scenario = ActivityScenario.launch(MainActivity::class.java)
         scenario.onActivity {
             it.tracking = false
-            it.bluetoothConnected = true
-            val storageDir = it.getExternalFilesDir("pcdfdata")
-            storageDir?.canWrite()
-            storageDir?.createNewFile().apply {
-                file = File(storageDir,"Dir")
-                file.mkdir()
-                val newFile = File(file, "testFile.ppcdf")
-                newFile.appendText(data)
-                newFile.canRead()
-                newFile.deleteOnExit()
-            }
+            it.bluetoothConnected = false
+            it.changeDonatingAllowed(false) // disable donating
         }
-
+        onView(withId(R.id.historyImageButton)).perform(click())
     }
 
+    /**
+     * Checks whether the history fragment is displayed correctly with correct title and data.
+     */
     @Test
     fun testHistoryFragmentDisplay() {
-        onView(withId(R.id.historyImageButton)).perform(ViewActions.click())
-        onData(Matchers.allOf(Matchers.containsString("testFile")))
-            .perform(click())
-        onView(withId(R.id.textViewHistory)).check(matches(withText("History")))
+        onView(withId(R.id.title_textview)).check(matches(withText("History")))
+        onData(Matchers.allOf(Matchers.containsString("A20-1")))
+            .check(matches(isDisplayed()))
     }
+
+    /**
+     * Checks whether you can can get detailed information about a PCDF file by clicking on it.
+     * TODO: check why library is not found
+     */
+    @Test
+    fun testHistoryNavigation() {
+        onData(Matchers.allOf(Matchers.containsString("A20-1")))
+            .perform(click())
+        onView(withId(R.id.tabEventData)).check(matches(withText("Event Log")))
+        onView(withId(R.id.tabSpeedProfile)).check(matches(withText("Speed Profile")))
+        onView(withId(R.id.tabRDEResult)).check(matches(withText("RDE Result")))
+    }
+
 }
